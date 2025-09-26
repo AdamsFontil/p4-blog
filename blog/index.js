9d07fd9c843b55929a9de8d8545aad972dea0431
Adams <adamsfuntil@gmail.com>
2025-09-25 22:24:35 -0400

    4.2 done

--- blog/index.js
+++ blog/index.js
@@ -1,40 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://adamsfuntil_db_user:BV9Ajub4BQELdslZ@cluster0.ntim1gf.mongodb.net/BlogApp?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
-console.log('connected to', mongoUrl)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-    console.log('getting blogs,', blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = process.env.PORT || 3005
-console.log('what prt am i using', PORT)
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
