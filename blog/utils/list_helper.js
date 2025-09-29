b83a03b3fae0c7e4c35b0221c77ddcde7c40e917
Adams <adamsfuntil@gmail.com>
2025-09-29 11:45:28 -0400

    4.4

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -3,6 +3,40 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+// const average = array => {
+//   const reducer = (sum, item) => {
+//     return sum + item
+//   }
+
+//   return array.length === 0
+//     ? 0
+//     : array.reduce(reducer, 0) / array.length
+// }
+
+
+const totalLikes = (blogs) => {
+  // console.log('what do where have here',blogs)
+  // console.log('blogs length is', blogs.length)
+
+  if (blogs.length === 0) {
+    console.log('empty list returning 0')
+    return 0
+  } else {
+    const total = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    console.log('total likes are ...',total)
+    return total
+
+  }
+
+}
+
+
+
+
+
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
