e32f3a0dfd33f4b4b6d2308963b6099f1baf7225
Adams <adamsfuntil@gmail.com>
2025-10-01 05:59:58 -0400

    -4.9 done

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -8,6 +8,19 @@ blogsRouter.get('/', async (request, response) => {
 
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end
+  }
+
+})
+
+
+
+
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -12,15 +12,33 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('all blogs are returned', async () => {
+test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
   console.log(`response body length is ${response.body.length} vs contant length is ${response.body.length}`)
+
+
+})
+test.only('id string is the identifier', async () => {
+  const blogs = await Blog.find({})
+  const specificBlog = blogs[2].toJSON()
+
+  console.log('need this blog\'s id:', specificBlog.id)
+
+  const callSpecificBlog = await api
+    .get(`/api/blogs/${specificBlog.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.ok(callSpecificBlog.body.id, 'Expected "id" to be defined')
+  assert.strictEqual(typeof callSpecificBlog.body.id, 'string', '"id" should be a string')
+  assert.strictEqual(callSpecificBlog.body._id, undefined, 'Expected "_id" to be undefined')
+  console.log('confirm that calling specific blog results in id instead _id also _v should be missing', callSpecificBlog.body )
 })
