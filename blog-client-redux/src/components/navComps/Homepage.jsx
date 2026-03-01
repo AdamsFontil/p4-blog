0658a521737cb4d9f6f5578880e712b3529e6806
adams <108461931+AdamsFontil@users.noreply.github.com>
2026-02-28 23:55:13 -0500

    deploy only on merges

--- blog-client-redux/src/components/navComps/Homepage.jsx
+++ blog-client-redux/src/components/navComps/Homepage.jsx
@@ -1,19 +1,19 @@
-import Blog from '../defaultComps/Blog'
-import Togglable from '../defaultComps/Togglable'
-import CreateBlogForm from '../defaultComps/CreateBlogForm'
-import { useSelector } from 'react-redux'
+import Blog from "../defaultComps/Blog";
+import Togglable from "../defaultComps/Togglable";
+import CreateBlogForm from "../defaultComps/CreateBlogForm";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  const blogs = useSelector((state) => state.blogs)
+  const blogs = useSelector((state) => state.blogs);
 
-  console.log('blogs from home', blogs)
+  console.log("blogs from home", blogs);
   return (
     <div className="bg-rd-800">
       <Togglable buttonLabelShow="create new blog" buttonLabelCancel="cancel">
         <CreateBlogForm />
       </Togglable>
       <div className="p-4">
-        <h1>Hello Wrld 2</h1>
+        <h1>Hello Wrld 3</h1>
         <ul className="list bg-base-100 shadow-md">
           {[...blogs]
             .sort((a, b) => b.likes - a.likes)
@@ -23,7 +23,7 @@ const Home = () => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
